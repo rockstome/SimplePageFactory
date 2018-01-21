edf34f8701f67c2dd922ffca8e20901b8686cfa6
tomasz solarz <tomasz.solarz@gmail.com>
2018-01-21 17:33:18 +0100

    add extension methods for webelements

---
+++ Helpers/WebElementHelper.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+
+namespace SimplePageFactory.Helpers
+{
+    public static class WebElementHelper
+    {
+        /// <summary>
+        /// When checkbox is off clicks on it, otherwise no action is made.
+        /// </summary>
+        public static void CheckboxOn(this IWebElement element)
+        {
+            if (!element.Selected)
+                element.Click();
+        }
+
+        /// <summary>
+        /// When checkbox is on clicks on it, otherwise no action is made.
+        /// </summary>
+        public static void CheckboxOff(this IWebElement element)
+        {
+            if (element.Selected)
+                element.Click();
+        }
+    }
+}

2ffb7c1c9993eab09dbbf3c62b7673951c8d0913
tomasz solarz <tomasz.solarz@gmail.com>
2018-02-19 22:52:03 +0100

    commit from git gui

--- Helpers/WebElementHelper.cs
+++ Helpers/WebElementHelper.cs
@@ -86,9 +86,29 @@ namespace SimplePageFactory.Helpers
             executor.ExecuteScript("return arguments[0].focus();", element);
         }
 
+        // TODO: check if works also with textareas
+        /// <summary>
+        /// Gets value from this input.
+        /// </summary>
+        /// <param name="driver">The driver to execute JavaScript script.</param>
+        /// <returns>Value from input.</returns>
+        public static string JsGetInputValue(this IWebElement element, IWebDriver driver)
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            return (string)executor.ExecuteScript("return arguments[0].value", element);
+        }
+
+        /// <summary>
+        /// Sets red solid border on parent node.
+        /// </summary>
+        /// <param name="driver"></param>
+        public static void JsHighlightParent(this IWebElement element, IWebDriver driver)
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("return arguments[0].parentNode.style.border='3px solid red'", element);
+        }
 
         // https://www.w3schools.com/jsref/dom_obj_all.asp
-        // find element
         // get the value of an element
         // highlight an element
     }
---
+++ Tests/_FrameworkTests/WebElementHelpers.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SimplePageFactory.Helpers;
+
+namespace SimplePageFactory.Tests._FrameworkTests
+{
+    [TestFixture]
+    public class WebElementHelpers
+    {
+        IWebDriver d;
+        IWebElement e;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            d = new ChromeDriver();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            d.Navigate().GoToUrl("about:blank");
+        }
+
+        [Test, Category("Framework")]
+        public void JsGetInputValue()
+        {
+            d.Navigate().GoToUrl(@"data:text/html,<div><input id='input' ></input>");
+            e = d.FindElement(By.Id("input"));
+
+            // read input value by js and compare to sended text
+            string text = "some text";
+            e.SendKeys(text);
+            Assert.That(e.JsGetInputValue(d), Is.EqualTo(text));
+
+            // clear, read value by js and compare with empty string
+            e.Clear();
+            Assert.That(e.JsGetInputValue(d), Is.EqualTo(string.Empty));
+        }
+
+        [Test, Category("Framework")]
+        public void JsClear()
+        {
+            d.Navigate().GoToUrl(@"data:text/html,<div><input id='input' ></input>");
+            e = d.FindElement(By.Id("input"));
+
+            // check if text is in input
+            string text = "some text";
+            e.SendKeys(text);
+            Assert.That(e.GetAttribute("value"), Is.EqualTo(text));
+
+            // check if input is empty
+            e.JsClear(d);
+            Assert.That(e.GetAttribute("value"), Is.EqualTo(string.Empty));
+        }
+
+        [Test, Category("Framework")]
+        public void JsSendKeys()
+        {
+            d.Navigate().GoToUrl(@"data:text/html,<div><input id='input' ></input>");
+            e = d.FindElement(By.Id("input"));
+
+            // check if text is in input
+            string text = "some text";
+            e.JsSendKeys(d, text);
+            Assert.That(e.GetAttribute("value"), Is.EqualTo(text));
+        }
+
+        [Test, Category("Framework")]
+        public void JsHighlightParent()
+        {
+            d.Navigate().GoToUrl(@"data:text/html,<div><h1>Hello</h1><h1 id='h1'>World</h1>");
+            e = d.FindElement(By.Id("h1"));
+
+            IWebElement parentNode = d.FindElement(By.XPath("//*[@id='h1']/.."));
+
+            // check if parent has no style
+            Assert.That(parentNode.GetAttribute("style"), Is.EqualTo(string.Empty));
+
+            // check if parent has style
+            e.JsHighlightParent(d);
+            Assert.That(parentNode.GetAttribute("style"), Is.EqualTo("border: 3px solid red;"));
+
+            // check if we cant still use this element
+            Assert.That(e.Text, Is.EqualTo("World"));
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            d.Quit();
+        }
+    }
+}

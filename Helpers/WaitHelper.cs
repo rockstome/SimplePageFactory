320f68d1abdc988dd05cc6481c343e2835be5e95
tomasz solarz <tomasz.solarz@gmail.com>
2018-02-28 21:54:33 +0100

    WaitHelper v1

---
+++ Helpers/WaitHelper.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SimplePageFactory.Helpers
+{
+    public class WaitHelper
+    {
+        public static void WaitForTextPresentedInField(IWebDriver d, IWebElement e, string text, int time = 10)
+        {
+            new WebDriverWait(d, TimeSpan.FromSeconds(time)).Until(TextPresentedInField(e, text));
+        }
+
+        public static string  WaitForTextPresentedInField2(IWebDriver d, IWebElement e, string text, int time = 30)
+        {
+            return new WebDriverWait(d, TimeSpan.FromSeconds(time)).Until(TextPresentedInField(e, text));
+        }
+
+        public static Func<IWebDriver, string> TextPresentedInField(IWebElement e, string text)
+        {
+            return (d) => {
+                var value = e.GetAttribute("value");
+                if (value.Length != 0)
+                    return value;
+                return null;
+            };
+        }
+
+        [Test]
+        public void Test()
+        {
+            var d = new ChromeDriver();
+            d.Navigate().GoToUrl("data:text/html,<input id='input' value=''/>");
+            IWebElement e = d.FindElement(By.Id("input"));
+            WaitHelper.WaitForTextPresentedInField2(d, e, "elo");
+            //WaitForTextPresentedInField(d, By.Id("input"), "elo");
+        }
+    }
+}

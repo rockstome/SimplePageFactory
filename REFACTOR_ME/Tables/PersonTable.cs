0f42a6650681ab2d9fa26efd4e569637305fece9
tomasz solarz <tomasz.solarz@gmail.com>
2018-03-18 18:39:20 +0100

    tables v2

--- REFACTOR_ME/Tables/PersonTable.cs
+++ REFACTOR_ME/Tables/PersonTable.cs
@@ -16,6 +16,10 @@ namespace SimplePageFactory.REFACTOR_ME.Tables
             this.table = table;
         }
 
+        /// <summary>
+        /// Method to get list of table headers.
+        /// </summary>
+        /// <returns>List of table headers.</returns>
         public List<string> GetHeaders()
         {
             List<string> headers = new List<string>();
@@ -25,6 +29,15 @@ namespace SimplePageFactory.REFACTOR_ME.Tables
             }
             return headers;
         }
+
+        /// <summary>
+        /// Method to get number of rows.
+        /// </summary>
+        /// <returns>Number of rows.</returns>
+        public int GetTotalRows()
+        {
+            return table.FindElement(By.TagName("tbody")).FindElements(By.TagName("tr")).Count;
+        }
     }
 
     public class PersonTable : AbstractTable
@@ -33,6 +46,10 @@ namespace SimplePageFactory.REFACTOR_ME.Tables
         {
         }
 
+        /// <summary>
+        /// Method to get all table rows.
+        /// </summary>
+        /// <returns>List of Person class objects.</returns>
         public List<Person> GetRows()
         {
             List<Person> rows = new List<Person>();
@@ -51,6 +68,54 @@ namespace SimplePageFactory.REFACTOR_ME.Tables
 
             return rows;
         }
+
+        /// <summary>
+        /// Method to get a table row element.
+        /// </summary>
+        /// <param name="index">The row index. Star from 1.</param>
+        /// <returns>Person who meets the criteria.</returns>
+        public Person GetRow(int index)
+        {
+            if (0 < index && index <= GetTotalRows())
+            {
+                var row = table
+                    .FindElement(By.TagName("tbody"))
+                    .FindElement(By.XPath($".//tr[{index}]"));
+                return Person.Mapper(new List<IWebElement>(row.FindElements(By.XPath(".//td"))));
+            }
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+
+        /// <summary>
+        /// Method to get a table row that contains specific query.
+        /// </summary>
+        /// <param name="query">The query text to search for.</param>
+        /// <returns>Person who meets the criteria.</returns>
+        public Person GetRow(string query)
+        {
+            var row = table
+                .FindElement(By.TagName("tbody"))
+                .FindElement(By.XPath($".//td[contains(.,'{query}')]//parent::tr"));
+            return Person.Mapper(new List<IWebElement>(row.FindElements(By.XPath(".//td"))));
+        }
+
+        /// <summary>
+        /// Method to get the person who has specific balance.
+        /// </summary>
+        /// <param name="balance">
+        /// Balance in format that was mapped, example: "100 PLN".
+        /// </param>
+        /// <returns>Person who meets the criteria.</returns>
+        public Person GetRowByBalance(string balance)
+        {
+            // TODO: on large tables this sucks
+            var persons = GetRows();
+            foreach (var person in persons)
+                if (person.GetBalance().Contains(balance))
+                    return person;
+            throw new NotFoundException();
+        }
     }
 
     public class Person
@@ -58,57 +123,72 @@ namespace SimplePageFactory.REFACTOR_ME.Tables
         private int id;
         private string name;
         private string surname;
-        private List<IWebElement> sites;
-        private string amount;
+        private List<IWebElement> links;
+        private string balance;
 
-        public Person(int id, string name, string surname, List<IWebElement> sites, string amount)
+        public int GetId() => id;
+        public string GetName() => name;
+        public string GetSurname() => surname;
+        /// <param name="index">Start from 1.</param>
+        public IWebElement GetLink(int index) => links[index - 1];
+        public string GetBalance() => balance;
+
+        public Person(int id, string name, string surname, List<IWebElement> links, string balance)
         {
             this.id = id;
             this.name = name;
             this.surname = surname;
-            this.sites = sites;
-            this.amount = amount;
+            this.links = links;
+            this.balance = balance;
         }
 
         public override string ToString()
         {
-            var hrefs = sites.Select(s => s.GetAttribute("href")).ToList<string>();
-            return $"new Person({id},\"{name}\",\"{surname}\",{hrefs.Count},{amount})";
+            var hrefs = links.Select(s => s.GetAttribute("href")).ToList();
+            return $"new Person({id},\"{name}\",\"{surname}\",{hrefs.Count},{balance})";
         }
 
         public static Func<List<IWebElement>, Person> Mapper =
             (cells) =>
             {
+                int id = int.Parse(cells[0].Text);
                 var links = cells[3].FindElements(By.XPath(".//a"));
-
+                var divs = cells[4].FindElements(By.XPath(".//div"));
+                string balance = string.Join(" ", divs.Select(e => e.Text));
+                
                 return new Person(
-                int.Parse(cells[0].Text),
+                id,
                 cells[1].Text,
                 cells[2].Text,
                 new List<IWebElement>(links),
-                cells[4].FindElement(By.XPath(".//div[1]")).Text + " " + cells[4].FindElement(By.XPath(".//div[2]")).Text);
+                balance);
             };
     }
 
     public class PersonTest
     {
         [Test]
-        public void Test1()
+        public void BasicActions()
         {
             IWebDriver d = new ChromeDriver();
-            d.Navigate().GoToUrl("C:/Users/tomas/Documents/Visual%20Studio%202015/Projects/Tomasz/SimplePageFactory/Sites/table.html");
-            var personTable = new PersonTable(d.FindElement(By.Id("table")));
+            d.Navigate().GoToUrl("C:/Users/tomas/Documents/Visual%20Studio%202015/" +
+                "Projects/Tomasz/SimplePageFactory/Sites/table.html");
+            var table = new PersonTable(d.FindElement(By.Id("table")));
 
-            foreach (var header in personTable.GetHeaders())
-                Console.Write(header + ", ");
-            Console.WriteLine();
+            // Number of rows.
+            Console.WriteLine(table.GetTotalRows());
 
-            foreach (var person in personTable.GetRows())
-                Console.WriteLine(person);
-        }
+            // Name of person in 1st row.
+            Console.WriteLine(table.GetRow(1).GetName());
+
+            // Balance of person in 2nd row.
+            Console.WriteLine(table.GetRow(2).GetBalance());
+
+            // Balance of Adam.
+            Console.WriteLine(table.GetRow("Adam").GetBalance());
+
+            // Click second link of person who have 99 PLN.
+            table.GetRowByBalance("99 PLN").GetLink(2).Click();
+        }        
     }
 }
-
-
-
-
